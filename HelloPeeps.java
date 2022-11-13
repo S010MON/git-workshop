994d881c1dec83aaaa44236e75872765addd768a
S010MON <leon_debnath@hotmail.co.uk>
2022-11-13 15:18:29 +0100

    Added images to folder

---
+++ HelloPeeps.java
@@ -0,0 +1,12 @@
+public class HelloPeeps
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello " + leonFunction() + "! How are you?");
+	}
+
+	public static String leonFunction()
+	{
+		return "Leon";
+	}
+}
